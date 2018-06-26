cf9772e29ae02c7eaa42ff53c2744f35426cbc84
Marek Suscak <marek.suscak@gmail.com>
2018-06-27 01:42:24 +0200

    Add a pointer to the thinking in React docs

--- src/index.js
+++ src/index.js
@@ -2,6 +2,9 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
 
+// Follow the tutorial:
+// https://reactjs.org/docs/thinking-in-react.html
+
 class App extends React.Component {
   render() {
     return (
